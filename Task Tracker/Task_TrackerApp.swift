dbd972a4f3b8e59b9af5f99f899dbeae6d7e88cc
Kushick <cbkushick@gmail.com>
2026-05-14 23:35:55 +0600

    Initial Commit

---
+++ Task Tracker/Task_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_TrackerApp.swift
+//  Task Tracker
+//
+//  Created by Kushick Chakraborty on 14/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Task_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
