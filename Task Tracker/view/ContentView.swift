2cadcadc2243c4e44d38ac36cf1145aabb6a8bf7
Kushick <cbkushick@gmail.com>
2026-05-16 22:03:23 +0600

    Removing LazyVStack

--- Task Tracker/view/ContentView.swift
+++ Task Tracker/view/ContentView.swift
@@ -20,7 +20,7 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack{
-            LazyVStack{
+            VStack{
                 
                 if tasks.isEmpty{
                     EmptyView()
